5a62fcd0c2ae69907ccc3796b43b7d89354b9a0a
Steven Ciraolo <steve@thrackle.io>
2025-03-26 20:57:32 -0700

    removed log import

--- src/amm/base/BaseEquations.sol
+++ src/amm/base/BaseEquations.sol
@@ -1,7 +1,6 @@
 // SPDX-License-Identifier: UNLICENSED
 pragma solidity ^0.8.24;
 import {MathLibs, packedFloat} from "../mathLibs/MathLibs.sol";
-import "forge-std/console2.sol";
 
 /**
  * @title Equations used by multiple TBC types
