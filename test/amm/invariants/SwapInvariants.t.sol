4e0c5924edbe282045477805a42679e0713abdfc
Palmerg4 <71123289+Palmerg4@users.noreply.github.com>
2025-03-28 15:52:07 -0400

    One last cleanup

--- test/amm/invariants/SwapInvariants.t.sol
+++ test/amm/invariants/SwapInvariants.t.sol
@@ -21,26 +21,29 @@ abstract contract SwapInvariants is TestCommonSetup {
         bytes4[] memory selectors = new bytes4[](1);
         selectors[0] = _handler.swap.selector;
         targetContract(address(_handler));
-        targetSelector(FuzzSelector({addr: address(_handler), selectors: selectors}));
+        targetSelector(
+            FuzzSelector({addr: address(_handler), selectors: selectors})
+        );
         targetSender(admin);
         vm.startPrank(admin);
         // Set initial X value to something above 0 before starting to swap for X
-        (uint expected, , ) = pool.simSwap(address(pool.yToken()), 1_000_000_000_000_000_000);
+        (uint expected, , ) = pool.simSwap(
+            address(pool.yToken()),
+            1_000_000_000_000_000_000
+        );
         pool.swap(address(pool.yToken()), 1_000_000_000_000_000_000, expected);
     }
-    function invariant_verifyAmountOutNeverExceedsLiquidity_TokenX() public view {
-        assertLt(_handler.trackedAmountOutX(), IERC20(pool.xToken()).balanceOf(address(pool)));
+    function invariant_verifyAmountOutNeverExceedsLiquidity_TokenX()
+        public
+        view
+    {
+        assertLt(
+            _handler.trackedAmountOutX(),
+            IERC20(pool.xToken()).balanceOf(address(pool))
+        );
     }
     function invariant_verifyFeesIncreaseTokenX() public {
         assertLt(lastFees, pool.collectedLPFees());
         lastFees = pool.collectedLPFees();
     }
-    // function invariant_cumulativePriceAndLastBlockTimestammpOnlyIncrease() public {
-    //     uint cumulativePrice = CumulativePrice(address(pool)).cumulativePrice();
-    //     uint blockTimestamp = CumulativePrice(address(pool)).lastBlockTimestamp();
-    //     assertGt(cumulativePrice, lastCumulativePrice, "Current cumulativePrice must be greater than last cumulativePrice");
-    //     assertGt(blockTimestamp, lastBlockTimestamp, "Current blockTimestamp must be greater than last blockTimestamp");
-    //     lastCumulativePrice = cumulativePrice;
-    //     lastBlockTimestamp = blockTimestamp;
-    // }
 }
