26b46f79ce8c06fa3aff65ed18513feb4ac9b4d4
Oscar Serna <oscar.eduardo.serna@gmail.com>
2025-04-09 16:45:56 -0500

    fixed test

--- test/amm/common/PoolCommon.t.u.sol
+++ test/amm/common/PoolCommon.t.u.sol
@@ -549,10 +549,14 @@ abstract contract PoolCommonTest is TestCommonSetup, PoolCommonAbs {
         (uint expected, uint feeAmount, ) = pool.simSwap(address(_yToken), 1_000 * fullToken);
 
         // we test that no disguised negative amount in can be traded.
+        _yToken.approve(address(pool), type(uint256).max);
         vm.expectRevert(
             abi.encodeWithSignature(
-                "SafeCastOverflowedUintToInt(uint256)",
-                115792089237316195423570985008687907853269984665640564039457584007913129629936
+                "ERC20InsufficientBalance(address,uint256,uint256)",
+                admin, 1e12 * fullToken, 
+                address(_yToken) == address(stableCoin) ? 
+                115792089237316195423570985008687907853269984665640564039457584007912129639936 : 
+                115792089237316195423570985008687907853269984665640564038457584007913129639936
             )
         );
         pool.swap(address(_yToken), uint(-int(1_000 * fullToken)), expected, address(0));
