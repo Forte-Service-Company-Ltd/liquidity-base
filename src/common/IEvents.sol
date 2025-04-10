3eb70845f22f5c61afb119f9f89c4e0428b1317a
Palmerg4 <71123289+Palmerg4@users.noreply.github.com>
2025-04-10 15:39:15 -0400

    Moved LiquidityDeposited event to IEvents in liquidity base

--- src/common/IEvents.sol
+++ src/common/IEvents.sol
@@ -8,8 +8,6 @@ import {packedFloat} from "../amm/mathLibs/MathLibs.sol";
  * @notice this file should be then inherited in the contract interfaces to use the events.
  */
 
-
-
 /**
  * @dev events common for the pool and the factory contract
  * @notice any change in this interface most likely means a breaking change with monitoring services
@@ -32,7 +30,15 @@ interface IPoolEvents is CommonEvents {
     event FeesCollected(FeeCollectionType indexed _feeType, address indexed _collector, uint256 indexed _amount);
     event Swap(address indexed _tokenIn, uint256 indexed _amountIn, uint256 indexed _amountOut, uint256 _minOut, address _recipient);
     event RevenueWithdrawn(address indexed _collector, uint256 indexed tokenId, uint256 indexed _amount, address _recipient);
-    event LiquidityWithdrawn(address lp, uint indexed tokenId, uint256 indexed amountOutXToken, uint256 indexed amountOutYToken, uint256 revenue, address _recipient);
+    event LiquidityWithdrawn(
+        address lp,
+        uint indexed tokenId,
+        uint256 indexed amountOutXToken,
+        uint256 indexed amountOutYToken,
+        uint256 revenue,
+        address _recipient
+    );
+    event LiquidityDeposited(address indexed _sender, uint256 indexed _tokenId, uint256 _A, uint256 _B);
     event LPTokenUpdated(uint256 indexed tokenId, packedFloat wj, packedFloat hn);
     event FeesGenerated(uint256 indexed lpFee, uint256 indexed protocolFee);
 }
