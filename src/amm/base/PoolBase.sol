894f011ca69007c3e53eeead1e8177d17bc09852
Oscar Serna <oscar.eduardo.serna@gmail.com>
2025-04-11 12:47:42 -0500

    using remappings now

--- src/amm/base/PoolBase.sol
+++ src/amm/base/PoolBase.sol
@@ -5,7 +5,7 @@ import {Pausable} from "../../../lib/openzeppelin-contracts/contracts/utils/Paus
 import {IERC20Metadata} from "../../../lib/openzeppelin-contracts/contracts/token/ERC20/extensions/IERC20Metadata.sol";
 import {IERC20} from "../../../lib/openzeppelin-contracts/contracts/token/ERC20/IERC20.sol";
 import {SafeERC20} from "../../../lib/openzeppelin-contracts/contracts/token/ERC20/utils/SafeERC20.sol";
-import {SafeCast} from "../../../lib/openzeppelin-contracts/contracts/utils/Math/SafeCast.sol";
+import {SafeCast} from "@openzeppelin/contracts/utils/Math/SafeCast.sol";
 import {IPool} from "./IPool.sol";
 import "../../common/IErrors.sol";
 import {CalculatorBase, packedFloat} from "./CalculatorBase.sol";
