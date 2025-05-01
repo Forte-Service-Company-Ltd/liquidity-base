2317557a68ffe73b57497836f0019b122683b0d4
Palmerg4 <71123289+Palmerg4@users.noreply.github.com>
2025-05-01 15:27:09 -0400

    LIQ-249 Update SPDX License Identifier for all src files (#89)
    * LIQ-249 Update SPDX License Identifier for all src files
    
    * Updated license identifier to use BUSL-1.1
    
    * Updated license identifier for all test/script files as well

--- src/common/IEvents.sol
+++ src/common/IEvents.sol
@@ -1,4 +1,4 @@
-// SPDX-License-Identifier: UNLICENSED
+// SPDX-License-Identifier: BUSL-1.1
 pragma solidity ^0.8.24;
 
 import {packedFloat} from "../amm/mathLibs/MathLibs.sol";
--- test/util/TestCommonSetupAbs.sol
+++ test/util/TestCommonSetupAbs.sol
@@ -1,4 +1,4 @@
-// SPDX-License-Identifier: UNLICENSED
+// SPDX-License-Identifier: BUSL-1.1
 pragma solidity ^0.8.17;
 
 import {PoolBase} from "src/amm/base/PoolBase.sol";
