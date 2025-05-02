4dfb52962b95067454df68e91ef35e0bed72d8ce
Steven Ciraolo <steve@thrackle.io>
2025-05-02 12:18:04 -0700

    increased LIFETIME_MAX_X

--- script/python/equations/calculate_last_revenue_claim.py
+++ script/python/equations/calculate_last_revenue_claim.py
@@ -7,7 +7,7 @@ from float_utils import normalize_and_print
 def calculate_last_revenue_claim(args):
     getcontext().prec = 200
     hn = Decimal(args.hn) / Decimal(1e18) 
-    wj = Decimal(args.wj) / Decimal(1e18)
+    wj = Decimal(args.wjMan) * (10 ** args.wjExp) if args.wjExp >= 0 else Decimal(args.wjMan) / (10 ** -args.wjExp)
     r_hat = Decimal(args.r_hat) / Decimal(1e18)
     w_hat = Decimal(args.w_hat) / Decimal(1e18)
 
@@ -16,8 +16,9 @@ def calculate_last_revenue_claim(args):
 
 def parse_args():
     parser = argparse.ArgumentParser()
+    parser.add_argument("wjMan", type=int)
+    parser.add_argument("wjExp", type=int)
     parser.add_argument("hn", type=int)
-    parser.add_argument("wj", type=int)
     parser.add_argument("r_hat", type=int)
     parser.add_argument("w_hat", type=int)
     
