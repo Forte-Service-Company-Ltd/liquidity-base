3435a60d68a82e8dcb71b741af5e47c9b9dbfbf3
Robin Polak <github@robinpolak.com>
2025-10-02 16:22:36 -0400

    Removing Necessist. (#112)
    Co-authored-by: Robin Polak <robin.polak@thrackle.io>

--- script/containers/necessist-run.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-set -e
-
-python3 -m venv .venv
-source .venv/bin/activate
-necessist --verbose --framework foundry -- --ffi
-
-date=$(date +"%Y-%m-%d")
-aws s3 cp necessist.db s3://necessist-database/aquifi-liquidity-necessist-${date}.db
